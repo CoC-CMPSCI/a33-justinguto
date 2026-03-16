b0573e14e92ed4a01f8cd17695b4c7fd3ae45e46
justinguto <163184050+justinguto@users.noreply.github.com>
2026-03-16 16:25:34 -0700

    Simplify variable declarations and conversion logic
    Refactor variable declarations and streamline input/output.

--- main.cpp
+++ main.cpp
@@ -4,15 +4,14 @@ using namespace std;
 
 int main()
 {
-    double C;
-    double F;
+    double C, F;
 
-    // TODO: [Step 1] Input a Celcius degree as a "double" type value
+    cin >> C;
 
-    // TODO: [Step 2] Convert it to a Fahrenheit degree using the Formula F = (9 / 5.0)* C + 32
-
+    F = (9 / 5.0) * C + 32;
     cout << setprecision(2) << fixed;
-    // TODO: [Step 3] Print the converted Fahrenheit
+
+    cout << F << endl;
 
     return 0;
 }
